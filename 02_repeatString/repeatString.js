65b66950fbbdf1c3bcc06ddb339d42c24fad2520
TeoFat <grasso.matteo@gmail.com>
2022-10-12 00:02:07 +0200

    01_helloWorld

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    let eco = '';
+    if (times >= 0) {
+        for (let i=0; i < times; i++) {
+            eco += string;
+        }
+    } else {
+        eco = 'ERROR';
+    }
+    return eco
 };
 
 // Do not edit below this line

e37d354c9ea9ca8d8bac3076881a680723f8e810
TeoFat <grasso.matteo@gmail.com>
2022-10-12 23:25:57 +0200

    Exercise 03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const reverse = new Array(string.length);
+    for (let i = 0; i < string.length; i++) {
+        reverse[string.length - i] = string[i];
+    }
+    return reverse.join('')
 };
 
 // Do not edit below this line
